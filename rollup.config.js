1bce342562ba259a93908bb6667fd32abee6adab
Matt Travi <programmer@travi.org>
2018-04-16 00:40:33 -0500

    build(package): defined the initial build process for the package

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+/* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+
+export default {
+  input: 'src/index.js',
+  external: [
+    'path',
+    'mz/fs',
+    'chalk',
+    'inquirer',
+    'lodash.uniq',
+    'shelljs'
+  ],
+  output: [
+    {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
+    {file: 'lib/index.es.js', format: 'es', sourcemap: true}
+  ]
+};
