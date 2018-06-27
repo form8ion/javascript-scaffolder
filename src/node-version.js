2acd8066de32b27c9616eb206ea36265bfc31cdc
Matt Travi <programmer@travi.org>
2018-06-26 22:53:18 -0500

    refactor(node-version): extracted request for the version
    to a separate module

---
+++ src/node-version.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+import exec from '../third-party-wrappers/exec-as-promised';
+
+export async function determineLatestVersionOf(nodeVersionCategory) {
+  const lowerCaseCategory = nodeVersionCategory.toLowerCase();
+  console.log(chalk.grey(`Determining ${lowerCaseCategory} version of node`));    // eslint-disable-line no-console
+
+  const nvmLsOutput = await exec(`. ~/.nvm/nvm.sh && nvm ls-remote${('LTS' === nodeVersionCategory) ? ' --lts' : ''}`);
+
+  const lsLines = nvmLsOutput.split('\n');
+  const lsLine = lsLines[lsLines.length - 2];
+
+  return lsLine.match(/(v[0-9]+\.[0-9]+\.[0-9]+)/)[1];
+}
---
+++ test/unit/node-version-test.js
@@ -0,0 +1,34 @@
+import sinon from 'sinon';
+import any from '@travi/any';
+import {assert} from 'chai';
+import * as exec from '../../third-party-wrappers/exec-as-promised';
+import {determineLatestVersionOf} from '../../src/node-version';
+
+suite('node version', () => {
+  let sandbox;
+  const version = `v${any.integer()}.${any.integer()}.${any.integer()}`;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(exec, 'default');
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that the latest node version is returned when latest is requested', async () => {
+    exec.default
+      .withArgs('. ~/.nvm/nvm.sh && nvm ls-remote')
+      .resolves([...any.listOf(any.word), version, ''].join('\n'));
+
+    assert.equal(await determineLatestVersionOf(any.word()), version);
+  });
+
+  test('that the latest lts node version is returned when LTS is requested', async () => {
+    exec.default
+      .withArgs('. ~/.nvm/nvm.sh && nvm ls-remote --lts')
+      .resolves([...any.listOf(any.word), version, ''].join('\n'));
+
+    assert.equal(await determineLatestVersionOf('LTS'), version);
+  });
+});
