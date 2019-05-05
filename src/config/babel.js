a8f3b4563a93223b15f6d6a41ba619acd38a5819
Matt Travi <programmer@travi.org>
2019-05-04 22:44:00 -0500

    feat(transpilation): added warnings when transpilation is not enabled

--- src/config/babel.js
+++ src/config/babel.js
@@ -1,9 +1,14 @@
 import {writeFile} from 'mz/fs';
+import {warn} from '@travi/cli-messages';
 
 export default async function ({projectRoot, preset, transpileLint}) {
-  if (false === transpileLint) return {devDependencies: [], scripts: {}, vcsIgnore: {files: [], directories: []}};
+  if (false === transpileLint) {
+    warn('Not configuring transpilation');
+    return {devDependencies: [], scripts: {}, vcsIgnore: {files: [], directories: []}};
+  }
 
   if (preset) await writeFile(`${projectRoot}/.babelrc`, JSON.stringify({presets: [preset.name]}));
+  else warn('Not configuring transpilation since no babel-config was provided');
 
   return {
     devDependencies: ['@babel/register', ...preset ? [preset.packageName] : []],
