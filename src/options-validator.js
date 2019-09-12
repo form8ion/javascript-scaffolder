bac290166d698d9239299dd6f385c404ae4afe2b
Matt Travi <programmer@travi.org>
2019-09-11 21:18:02 -0500

    refactor(joi): handled breaking changes

--- src/options-validator.js
+++ src/options-validator.js
@@ -2,66 +2,64 @@ import * as joi from '@hapi/joi';
 import hoek from '@hapi/hoek';
 
 export function validate(options) {
-  const {error, value} = joi.validate(
-    options,
-    joi.object().required()
-      .keys({
-        projectRoot: joi.string().required(),
-        projectName: joi.string().regex(/^@\w*\//, {invert: true}).required(),
-        visibility: joi.string().valid(['Public', 'Private']).required(),
-        license: joi.string().required(),
-        description: joi.string()
+  const schema = joi.object().required()
+    .keys({
+      projectRoot: joi.string().required(),
+      projectName: joi.string().regex(/^@\w*\//, {invert: true}).required(),
+      visibility: joi.string().valid('Public', 'Private').required(),
+      license: joi.string().required(),
+      description: joi.string()
+    })
+    .keys({
+      vcs: joi.object({
+        host: joi.string().required(),
+        owner: joi.string().required(),
+        name: joi.string().required()
       })
-      .keys({
-        vcs: joi.object({
-          host: joi.string().required(),
-          owner: joi.string().required(),
+    })
+    .keys({
+      configs: joi.object({
+        eslint: joi.object({scope: joi.string().regex(/^@[a-z0-9]+$/i, 'scope').required()}),
+        commitlint: joi.object({
+          packageName: joi.string().required(),
           name: joi.string().required()
+        }),
+        babelPreset: joi.object({
+          packageName: joi.string().required(),
+          name: joi.string().required()
+        }),
+        remark: joi.string()
+      }).default({})
+    })
+    .keys({
+      overrides: joi.object({
+        npmAccount: joi.string(),
+        author: joi.object({
+          name: joi.string().required(),
+          email: joi.string().email(),
+          url: joi.string().uri()
         })
-      })
-      .keys({
-        configs: joi.object({
-          eslint: joi.object({scope: joi.string().regex(/^@[a-z0-9]+$/i, 'scope').required()}),
-          commitlint: joi.object({
-            packageName: joi.string().required(),
-            name: joi.string().required()
-          }),
-          babelPreset: joi.object({
-            packageName: joi.string().required(),
-            name: joi.string().required()
-          }),
-          remark: joi.string()
-        }).default({})
-      })
-      .keys({
-        overrides: joi.object({
-          npmAccount: joi.string(),
-          author: joi.object({
-            name: joi.string().required(),
-            email: joi.string().email(),
-            url: joi.string().uri()
-          })
-        }).default({})
-      })
-      .keys({
-        ciServices: joi.object().pattern(/^/, joi.object({
-          scaffolder: joi.func().arity(1).required(),
-          public: joi.boolean(),
-          private: joi.boolean()
-        })).default({})
-      })
-      .keys({
-        hosts: joi.object().pattern(/^/, joi.object({
-          scaffolder: joi.func().arity(1).required(),
-          projectTypes: joi.array().items(joi.string().only(['static', 'node'])).default([])
-        })).default({})
-      })
-      .keys({
-        applicationTypes: joi.object().pattern(/^/, joi.object({
-          scaffolder: joi.func().arity(1).required()
-        })).default({})
-      })
-  );
+      }).default({})
+    })
+    .keys({
+      ciServices: joi.object().pattern(/^/, joi.object({
+        scaffolder: joi.func().arity(1).required(),
+        public: joi.boolean(),
+        private: joi.boolean()
+      })).default({})
+    })
+    .keys({
+      hosts: joi.object().pattern(/^/, joi.object({
+        scaffolder: joi.func().arity(1).required(),
+        projectTypes: joi.array().items(joi.string().valid('static', 'node')).default([])
+      })).default({})
+    })
+    .keys({
+      applicationTypes: joi.object().pattern(/^/, joi.object({
+        scaffolder: joi.func().arity(1).required()
+      })).default({})
+    });
+  const {error, value} = schema.validate(options);
 
   hoek.assert(!error, error);
 
