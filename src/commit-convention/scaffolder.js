a65246f3d8b8572909fff23aa231ff758c81a77f
Matt Travi <programmer@travi.org>
2020-12-06 16:07:39 -0600

    refactor(commit-convention): merged sub-results with deepmerge instead of manually

--- src/commit-convention/scaffolder.js
+++ src/commit-convention/scaffolder.js
@@ -1,3 +1,4 @@
+import deepmerge from 'deepmerge';
 import scaffoldCommitizen from '../config/commitizen';
 import scaffoldHusky from '../config/husky';
 import scaffoldCommitlint from './commitlint';
@@ -9,27 +10,26 @@ export default async function ({projectRoot, configs}) {
     configs.commitlint && scaffoldCommitlint({projectRoot, config: configs.commitlint})
   ]);
 
-  return {
-    devDependencies: [
-      ...commitizenResults.devDependencies,
-      ...huskyResults.devDependencies,
-      ...commitlintResults ? commitlintResults.devDependencies : []
-    ],
-    scripts: {...commitizenResults.scripts, ...huskyResults.scripts},
-    vcsIgnore: {files: [], directories: []},
-    badges: {
-      contribution: {
-        'commit-convention': {
-          img: 'https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg',
-          text: 'Conventional Commits',
-          link: 'https://conventionalcommits.org'
-        },
-        commitizen: {
-          img: 'https://img.shields.io/badge/commitizen-friendly-brightgreen.svg',
-          text: 'Commitizen friendly',
-          link: 'http://commitizen.github.io/cz-cli/'
+  return deepmerge.all([
+    commitizenResults,
+    huskyResults,
+    ...commitlintResults ? [commitlintResults] : [],
+    {
+      vcsIgnore: {files: [], directories: []},
+      badges: {
+        contribution: {
+          'commit-convention': {
+            img: 'https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg',
+            text: 'Conventional Commits',
+            link: 'https://conventionalcommits.org'
+          },
+          commitizen: {
+            img: 'https://img.shields.io/badge/commitizen-friendly-brightgreen.svg',
+            text: 'Commitizen friendly',
+            link: 'http://commitizen.github.io/cz-cli/'
+          }
         }
       }
     }
-  };
+  ]);
 }
