84a40a52f31a90c198e15d7517adb6dfd506d910
Matt Travi <programmer@travi.org>
2021-01-04 22:11:19 -0600

    test(yarn): configured appropriate verification of the installation command

--- test/integration/features/step_definitions/yarn-steps.js
+++ test/integration/features/step_definitions/yarn-steps.js
@@ -5,9 +5,10 @@ import {assertThatNpmConfigDetailsAreConfiguredCorrectlyFor} from './npm-steps';
 
 Given('the yarn cli is logged in', async function () {
   this.npmAccount = any.word();
+  td.when(this.execa(td.matchers.contains('. ~/.nvm/nvm.sh && nvm use && yarn add'))).thenResolve({stdout: ''});
 });
 
 Then('the yarn cli is configured for use', async function () {
   await assertThatNpmConfigDetailsAreConfiguredCorrectlyFor(this.projectType.toLowerCase());
-  td.verify(this.execa('yarn'));
+  td.verify(this.execa(td.matchers.contains('. ~/.nvm/nvm.sh && nvm use && yarn add')), {ignoreExtraArgs: true});
 });
