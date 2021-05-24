b681cae64d6ffbc8d4129c0929d63c1cc9b79293
Matt Travi <programmer@travi.org>
2021-05-24 16:35:11 -0500

    refactor(eslint): leveraged the extracted scaffolder

--- src/linting/eslint-test.js
+++ src/linting/eslint-test.js
@@ -1,145 +1,53 @@
 import {promises as fsPromises} from 'fs';
+import * as eslint from '@form8ion/eslint';
 import {assert} from 'chai';
 import any from '@travi/any';
 import sinon from 'sinon';
-import * as mkdir from '../../third-party-wrappers/make-dir';
 import scaffoldEsLint from './eslint';
 
 suite('eslint config scaffolder', () => {
   let sandbox;
   const packageName = any.word();
   const scope = any.string();
+  const projectRoot = any.string();
+  const buildDirectory = any.string();
+  const additionalConfigs = any.listOf(any.word);
+  const results = any.simpleObject();
 
   setup(() => {
     sandbox = sinon.createSandbox();
 
     sandbox.stub(fsPromises, 'writeFile');
-    sandbox.stub(mkdir, 'default');
+    sandbox.stub(eslint, 'scaffold');
   });
 
   teardown(() => sandbox.restore());
 
-  test('that the dependency is installed if the config is defined', async () => {
-    const result = await scaffoldEsLint({config: {scope}});
+  test('that initial config is created', async () => {
+    eslint.scaffold
+      .withArgs({scope, projectRoot, additionalConfigs, ignore: {directories: [`/${buildDirectory}/`]}})
+      .resolves(results);
 
-    assert.deepEqual(result.devDependencies, [`${scope}/eslint-config`]);
-  });
-
-  test('that the script is defined', async () => {
-    assert.deepEqual(
-      (await scaffoldEsLint({config: {packageName}})).scripts,
-      {
-        'lint:js': 'eslint . --cache',
-        'lint:js:fix': 'run-s lint:js -- --fix'
-      }
+    assert.equal(
+      await scaffoldEsLint({projectRoot, buildDirectory, config: {packageName, scope}, additionalConfigs}),
+      results
     );
   });
 
-  test('that the cache file is ignored from version control', async () => {
-    const result = await scaffoldEsLint({config: {packageName}});
+  test('that the coverage directory is excluded when the project is unit tested', async () => {
+    eslint.scaffold
+      .withArgs({scope, projectRoot, additionalConfigs, ignore: {directories: [`/${buildDirectory}/`, '/coverage/']}})
+      .resolves(results);
 
-    assert.deepEqual(result.vcsIgnore.files, ['.eslintcache']);
-  });
-
-  suite('config', () => {
-    const projectRoot = any.string();
-
-    test('that the base config is added to the root of the project if the config scope is provided', async () => {
-      await scaffoldEsLint({projectRoot, config: {packageName, scope}});
-
-      assert.calledWith(
-        fsPromises.writeFile,
-        `${projectRoot}/.eslintrc.yml`,
-        `root: true
-extends: '${scope}'`
-      );
-    });
-
-    test('that the additional configs are added if the config scope is provided', async () => {
-      const pathToCreatedDirectory = any.string();
-      const additionalConfigs = any.listOf(any.word);
-      mkdir.default.withArgs(`${projectRoot}/test/unit`).resolves(pathToCreatedDirectory);
-
-      const result = await scaffoldEsLint({
+    assert.equal(
+      await scaffoldEsLint({
         projectRoot,
+        buildDirectory,
         config: {packageName, scope},
-        unitTested: true,
-        additionalConfigs
-      });
-
-      assert.calledWith(
-        fsPromises.writeFile,
-        `${projectRoot}/.eslintrc.yml`,
-        `root: true
-extends:
-  - '${scope}'
-  - '${scope}/${additionalConfigs.join(`'\n  - '${scope}/`)}'`
-      );
-      assert.deepEqual(result.devDependencies, [`${scope}/eslint-config`]);
-    });
-
-    test('that no additional configs are added if the additional list is empty', async () => {
-      await scaffoldEsLint({projectRoot, config: {packageName, scope}, additionalConfigs: []});
-
-      assert.calledWith(
-        fsPromises.writeFile,
-        `${projectRoot}/.eslintrc.yml`,
-        `root: true
-extends: '${scope}'`
-      );
-    });
-
-    test('that a file pattern can be specified for a config', async () => {
-      const pathToCreatedDirectory = any.string();
-      const filePattern = any.string();
-      const configName = any.word();
-      const stringConfigs = any.listOf(any.word);
-      const additionalConfigs = [...stringConfigs, {name: configName, files: filePattern}];
-      mkdir.default.withArgs(`${projectRoot}/test/unit`).resolves(pathToCreatedDirectory);
-
-      const result = await scaffoldEsLint({
-        projectRoot,
-        config: {packageName, scope},
-        unitTested: true,
-        additionalConfigs
-      });
-
-      assert.calledWith(
-        fsPromises.writeFile,
-        `${projectRoot}/.eslintrc.yml`,
-        `root: true
-extends:
-  - '${scope}'
-  - '${scope}/${stringConfigs.join(`'\n  - '${scope}/`)}'
-
-overrides:
-  - files: ${filePattern}
-    extends: '${scope}/${configName}'
-`
-      );
-      assert.deepEqual(result.devDependencies, [`${scope}/eslint-config`]);
-    });
-
-    suite('eslint-ignore', () => {
-      test('that non-source files are excluded from linting', async () => {
-        const buildDirectory = any.string();
-
-        await scaffoldEsLint({projectRoot, config: {packageName, scope}, buildDirectory});
-
-        assert.calledWith(fsPromises.writeFile, `${projectRoot}/.eslintignore`, sinon.match(`/${buildDirectory}/`));
-        assert.neverCalledWith(fsPromises.writeFile, `${projectRoot}/.eslintignore`, sinon.match('/coverage/'));
-      });
-
-      test('that the coverage folder is excluded from linting when the project is unit tested', async () => {
-        await scaffoldEsLint({projectRoot, config: {packageName, scope}, unitTested: true});
-
-        assert.calledWith(
-          fsPromises.writeFile,
-          `${projectRoot}/.eslintignore`,
-          sinon.match(`
-/coverage/`)
-        );
-      });
-    });
+        additionalConfigs,
+        unitTested: true
+      }),
+      results
+    );
   });
 });
--- src/linting/eslint.js
+++ src/linting/eslint.js
@@ -1,40 +1,8 @@
-import {promises as fsPromises} from 'fs';
-
-function buildConfig(scope, additionalConfigs) {
-  const stringConfigs = additionalConfigs && additionalConfigs
-    .filter(additionalConfig => 'string' === typeof additionalConfig);
-  const complexConfigs = additionalConfigs && additionalConfigs
-    .filter(additionalConfig => 'object' === typeof additionalConfig);
-
-  const baseConfigs = stringConfigs && stringConfigs.length
-    ? `root: true\nextends:\n  - '${scope}'\n  - '${scope}/${stringConfigs.join(`'\n  - '${scope}/`)}'`
-    : `root: true\nextends: '${scope}'`;
-
-  return complexConfigs && complexConfigs.length
-    ? `${baseConfigs}
-
-overrides:${complexConfigs.map(complexConfig => `
-  - files: ${complexConfig.files}
-    extends: '${scope}/${complexConfig.name}'
-`)}`
-    : baseConfigs;
-}
+import {scaffold} from '@form8ion/eslint';
 
 export default async function ({config, projectRoot, unitTested, buildDirectory, additionalConfigs}) {
   const {scope} = config;
-  const eslintIgnoreDirectories = [`/${buildDirectory}/`, ...unitTested ? ['/coverage/'] : []];
+  const ignoreDirectories = [`/${buildDirectory}/`, ...unitTested ? ['/coverage/'] : []];
 
-  await Promise.all([
-    fsPromises.writeFile(`${projectRoot}/.eslintrc.yml`, buildConfig(scope, additionalConfigs)),
-    fsPromises.writeFile(`${projectRoot}/.eslintignore`, eslintIgnoreDirectories.join('\n'))
-  ]);
-
-  return {
-    devDependencies: [`${scope}/eslint-config`],
-    scripts: {
-      'lint:js': 'eslint . --cache',
-      'lint:js:fix': 'run-s lint:js -- --fix'
-    },
-    vcsIgnore: {files: ['.eslintcache']}
-  };
+  return scaffold({scope, projectRoot, additionalConfigs, ignore: {directories: ignoreDirectories}});
 }
