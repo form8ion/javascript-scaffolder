1350a5663a446267456285299f1465a05060242d
Matt Travi <programmer@travi.org>
2021-06-24 23:30:33 -0500

    refactor(npm-config): leveraged the official ini package rather than manually matching the format

--- src/config/npm.js
+++ src/config/npm.js
@@ -1,14 +1,16 @@
 import {promises as fs} from 'fs';
+import {stringify} from 'ini';
 import {projectTypes} from '@form8ion/javascript-core';
 
-export default function ({projectRoot, projectType}) {
-  const config = [
-    'update-notifier=false',
-    'engine-strict=true',
-    ...projectTypes.APPLICATION === projectType || projectTypes.CLI === projectType ? ['save-exact=true'] : []
-  ];
-
-  fs.writeFile(`${projectRoot}/.npmrc`, `${config.join('\n')}\n`);
+export default async function ({projectRoot, projectType}) {
+  await fs.writeFile(
+    `${projectRoot}/.npmrc`,
+    stringify({
+      'update-notifier': false,
+      'engine-strict': true,
+      ...(projectTypes.APPLICATION === projectType || projectTypes.CLI === projectType) && {'save-exact': true}
+    })
+  );
 
   return {scripts: {'lint:peer': 'npm ls >/dev/null'}};
 }
