dddffd5df7c5313894707641996d53072b8bbc10
Matt Travi <programmer@travi.org>
2021-07-06 16:46:07 -0500

    chore(peer-deps): properly enforced compatibility

--- example.js
+++ example.js
@@ -1,7 +1,6 @@
 // ##### Dependencies:
 import yargs from 'yargs';
 import {scaffold} from '@travi/project-scaffolder';
-import {scaffold as scaffoldTravisForJavaScript} from '@travi/travis-scaffolder-javascript';
 import {scaffold as scaffoldJavaScript} from './lib/index.cjs';
 
 // ##### Register with yargs
@@ -19,7 +18,7 @@ yargs
           commitlint: {name: '@form8ion', packageName: '@form8ion/commitlint-config'}
         },
         overrides: {npmAccount: 'form8ion'},
-        ciServices: {Travis: {scaffolder: scaffoldTravisForJavaScript, public: true}}
+        ciServices: {}
       })
     },
     overrides: {copyrightHolder: 'Matt Travi'}
