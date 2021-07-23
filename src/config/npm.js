a25f346a23c12bfe695462626c87cf24bd82832c
Matt Travi <programmer@travi.org>
2021-07-22 22:31:29 -0500

    refactor(save-exact): explained the reasoning for the save-exact config

--- src/config/npm.js
+++ src/config/npm.js
@@ -2,13 +2,17 @@ import {promises as fs} from 'fs';
 import {stringify} from 'ini';
 import {projectTypes} from '@form8ion/javascript-core';
 
+function projectWillNotBeConsumed(projectType) {
+  return projectTypes.APPLICATION === projectType || projectTypes.CLI === projectType;
+}
+
 export default async function ({projectRoot, projectType, registries}) {
   await fs.writeFile(
     `${projectRoot}/.npmrc`,
     stringify({
       'update-notifier': false,
       'engine-strict': true,
-      ...(projectTypes.APPLICATION === projectType || projectTypes.CLI === projectType) && {'save-exact': true},
+      ...projectWillNotBeConsumed(projectType) && {'save-exact': true},
       ...registries
         && Object.fromEntries(Object.entries(registries).map(([scope, url]) => ([`@${scope}:registry`, url])))
     })
