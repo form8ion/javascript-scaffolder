3a5e451c5aa9c6548d40268541c45357d761227a
Matt Travi <programmer@travi.org>
2021-07-23 01:48:49 -0500

    feat(dialects): only preset babel as an option if a preset is provided

--- src/dialects/prompt-choices-test.js
+++ src/dialects/prompt-choices-test.js
@@ -1,15 +1,23 @@
 import {dialects} from '@form8ion/javascript-core';
 import {assert} from 'chai';
+import any from '@travi/any';
 import buildDialectChoices from './prompt-choices';
 
 suite('dialect prompt questions', () => {
   test('that the available dialects are listed', () => {
     assert.deepEqual(
-      buildDialectChoices(),
+      buildDialectChoices({...any.simpleObject(), babelPreset: any.simpleObject()}),
       [
         {name: 'Common JS (no transpilation)', value: dialects.COMMON_JS, short: 'cjs'},
         {name: 'Modern JavaScript (transpiled)', value: dialects.BABEL, short: 'modern'}
       ]
+    );
+  });
+
+  test('that `babel` is not included in the choices list when a preset is not provided', () => {
+    assert.deepEqual(
+      buildDialectChoices(any.simpleObject()),
+      [{name: 'Common JS (no transpilation)', value: dialects.COMMON_JS, short: 'cjs'}]
     );
   });
 });
