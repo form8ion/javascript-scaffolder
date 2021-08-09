b89082fa1cc384c942a92c099a0b1ef8253d40ac
Matt Travi <developer@travi.org>
2021-08-09 15:39:38 -0500

    feat(dialects): enabled choosing the ts dialect if configs are provided

--- src/dialects/prompt-choices-test.js
+++ src/dialects/prompt-choices-test.js
@@ -6,15 +6,16 @@ import buildDialectChoices from './prompt-choices';
 suite('dialect prompt questions', () => {
   test('that the available dialects are listed', () => {
     assert.deepEqual(
-      buildDialectChoices({...any.simpleObject(), babelPreset: any.simpleObject()}),
+      buildDialectChoices({...any.simpleObject(), babelPreset: any.simpleObject(), typescript: any.simpleObject()}),
       [
         {name: 'Common JS (no transpilation)', value: dialects.COMMON_JS, short: 'cjs'},
-        {name: 'Modern JavaScript (transpiled)', value: dialects.BABEL, short: 'modern'}
+        {name: 'Modern JavaScript (transpiled)', value: dialects.BABEL, short: 'modern'},
+        {name: 'TypeScript', value: dialects.TYPESCRIPT, short: 'ts'}
       ]
     );
   });
 
-  test('that `babel` is not included in the choices list when a preset is not provided', () => {
+  test('that `babel` and `typescript` are not included in the choices list when configs are not provided', () => {
     assert.deepEqual(
       buildDialectChoices(any.simpleObject()),
       [{name: 'Common JS (no transpilation)', value: dialects.COMMON_JS, short: 'cjs'}]
