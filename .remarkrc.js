6282a888d3c5c47f50bcada688ac4cff17e2db04
Matt Travi <programmer@travi.org>
2021-08-17 15:11:09 -0500

    chore(remark): updated to the latest alpha of the remark preset

--- .remarkrc.js
+++ .remarkrc.js
@@ -9,6 +9,6 @@ exports.settings = {
 
 exports.plugins = [
   'remark-preset-lint-travi',
-  [require('remark-toc'), {tight: true}],
+  ['remark-toc', {tight: true}],
   ['remark-usage', {heading: 'example'}]
 ];
