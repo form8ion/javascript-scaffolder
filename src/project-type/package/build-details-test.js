febc9a7e8c7c09549744beb9277c329bb6141f02
Matt Travi <programmer@travi.org>
2021-09-23 14:30:08 -0500

    refactor(package): extracted build-details logic to a separate module

---
+++ src/project-type/package/build-details-test.js
@@ -0,0 +1,97 @@
+import {promises as fsPromises, promises as fs} from 'fs';
+import mustache from 'mustache';
+import {dialects} from '@form8ion/javascript-core';
+import {assert} from 'chai';
+import sinon from 'sinon';
+import any from '@travi/any';
+import * as touch from '../../../third-party-wrappers/touch';
+import * as mkdir from '../../../third-party-wrappers/make-dir';
+import * as rollupScaffolder from '../../build/rollup';
+import * as templatePath from '../../template-path';
+import buildDetails from './build-details';
+import * as camelcase from '../../../third-party-wrappers/camelcase';
+
+suite('package build details', () => {
+  let sandbox;
+  const projectRoot = any.string();
+  const projectName = any.word();
+  const pathToExample = `${projectRoot}/example.js`;
+  const pathToCreatedSrcDirectory = any.string();
+  const rollupResults = any.simpleObject();
+  const exampleContent = any.string();
+  const pathToExampleTemplate = any.string();
+  const exampleTemplateContent = any.string();
+  const camelizedProjectName = any.word();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(fs, 'writeFile');
+    sandbox.stub(fs, 'readFile');
+    sandbox.stub(touch, 'default');
+    sandbox.stub(mkdir, 'default');
+    sandbox.stub(templatePath, 'default');
+    sandbox.stub(rollupScaffolder, 'scaffold');
+    sandbox.stub(mustache, 'render');
+    sandbox.stub(camelcase, 'default');
+
+    mkdir.default.withArgs(`${projectRoot}/src`).resolves(pathToCreatedSrcDirectory);
+    rollupScaffolder.scaffold.withArgs({projectRoot}).resolves(rollupResults);
+    templatePath.default.withArgs('example.mustache').returns(pathToExampleTemplate);
+    fsPromises.readFile.withArgs(pathToExampleTemplate, 'utf8').resolves(exampleTemplateContent);
+    camelcase.default.withArgs(projectName).returns(camelizedProjectName);
+    mustache.render.withArgs(exampleTemplateContent, {projectName: camelizedProjectName}).returns(exampleContent);
+  });
+
+  teardown(() => sandbox.restore());
+
+  test('that common-js project is defined correctly', async () => {
+    assert.deepEqual(await buildDetails({dialect: dialects.COMMON_JS, projectRoot, projectName}), {});
+    assert.calledWith(fsPromises.writeFile, pathToExample, `const ${camelizedProjectName} = require('.');\n`);
+    assert.calledWith(touch.default, `${projectRoot}/index.js`);
+  });
+
+  test('that a modern-js project is defined correctly', async () => {
+    const results = await buildDetails({dialect: dialects.BABEL, projectRoot, projectName});
+
+    assert.deepEqual(
+      results,
+      {
+        ...rollupResults,
+        devDependencies: ['rimraf'],
+        scripts: {
+          clean: 'rimraf ./lib',
+          prebuild: 'run-s clean',
+          build: 'npm-run-all --print-label --parallel build:*',
+          prepack: 'run-s build'
+        },
+        vcsIgnore: {directories: ['/lib/']},
+        buildDirectory: 'lib',
+        badges: {consumer: {}}
+      }
+    );
+    assert.calledWith(touch.default, `${pathToCreatedSrcDirectory}/index.js`);
+    assert.calledWith(fsPromises.writeFile, pathToExample, exampleContent);
+  });
+
+  test('that the runkit badge is included for public projects', async () => {
+    const packageName = any.word();
+
+    const {badges} = await buildDetails({
+      dialect: dialects.BABEL,
+      projectRoot,
+      projectName,
+      packageName,
+      visibility: 'Public'
+    });
+
+    assert.deepEqual(
+      badges.consumer.runkit,
+      {
+        img: `https://badge.runkitcdn.com/${packageName}.svg`,
+        text: `Try ${packageName} on RunKit`,
+        link: `https://npm.runkit.com/${packageName}`
+      }
+    );
+  });
+});
---
+++ src/project-type/package/build-details.js
@@ -0,0 +1,67 @@
+import {promises as fs} from 'fs';
+import deepmerge from 'deepmerge';
+import mustache from 'mustache';
+import {dialects} from '@form8ion/javascript-core';
+import camelcase from '../../../third-party-wrappers/camelcase';
+import mkdir from '../../../third-party-wrappers/make-dir';
+import touch from '../../../third-party-wrappers/touch';
+import determinePathToTemplateFile from '../../template-path';
+import {scaffold as scaffoldRollup} from '../../build/rollup';
+
+const defaultBuildDirectory = 'lib';
+
+async function createExample(projectRoot, projectName) {
+  return fs.writeFile(
+    `${projectRoot}/example.js`,
+    mustache.render(
+      await fs.readFile(determinePathToTemplateFile('example.mustache'), 'utf8'),
+      {projectName: camelcase(projectName)}
+    )
+  );
+}
+
+async function buildDetailsForCommonJsProject({projectRoot, projectName}) {
+  await Promise.all([
+    touch(`${projectRoot}/index.js`),
+    fs.writeFile(`${projectRoot}/example.js`, `const ${camelcase(projectName)} = require('.');\n`)
+  ]);
+
+  return {};
+}
+
+export default async function ({projectRoot, projectName, visibility, packageName, dialect}) {
+  if (dialects.COMMON_JS === dialect) return buildDetailsForCommonJsProject({projectRoot, projectName});
+
+  const pathToCreatedSrcDirectory = await mkdir(`${projectRoot}/src`);
+  const [rollupResults] = await Promise.all([
+    scaffoldRollup({projectRoot}),
+    await createExample(projectRoot, projectName),
+    touch(`${pathToCreatedSrcDirectory}/index.js`)
+  ]);
+
+  return deepmerge(
+    rollupResults,
+    {
+      devDependencies: ['rimraf'],
+      scripts: {
+        clean: `rimraf ./${defaultBuildDirectory}`,
+        prebuild: 'run-s clean',
+        build: 'npm-run-all --print-label --parallel build:*',
+        prepack: 'run-s build'
+      },
+      vcsIgnore: {directories: [`/${defaultBuildDirectory}/`]},
+      buildDirectory: defaultBuildDirectory,
+      badges: {
+        consumer: {
+          ...'Public' === visibility && {
+            runkit: {
+              img: `https://badge.runkitcdn.com/${packageName}.svg`,
+              text: `Try ${packageName} on RunKit`,
+              link: `https://npm.runkit.com/${packageName}`
+            }
+          }
+        }
+      }
+    }
+  );
+}
