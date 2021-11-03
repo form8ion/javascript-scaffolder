0e64618fe0262657e9fb6a174da85ae549b5d4a7
Matt Travi <programmer@travi.org>
2021-11-02 20:33:36 -0500

    fix(question-names): referenced the exported names from the proper package
    after the unit-testing scaffolder was moved from the core package to the js package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import {questionNames as languageScaffolderPromptsQuestionNames} from '@travi/language-scaffolder-prompts';
-import {questionNames as coreQuestionNames} from '@form8ion/javascript-core';
+import {questionNames as jsQuestionNames} from '@form8ion/javascript';
 import {questionNames as jsScaffolderQuestionNames} from './prompts/question-names';
 
 export * from './scaffolder';
 export const questionNames = {
-  ...coreQuestionNames,
+  ...jsQuestionNames,
   ...languageScaffolderPromptsQuestionNames,
   ...jsScaffolderQuestionNames
 };
